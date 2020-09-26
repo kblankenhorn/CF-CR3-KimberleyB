e3754763d5e9090dbba5793f285d162a5d32fd19
Kimberley Blankenhorn <kvandlen@yahoo.com>
2020-09-26 11:50:16 +0200

    code review 3

---
+++ CR3.js
@@ -0,0 +1,43 @@
+
+function calcInsurance(){	
+
+var name = document.getElementById("name").value;
+console.log(name);
+var age =document.getElementById("age").value;
+console.log(age);
+var country =document.getElementById("country").value;
+console.log(country);
+var hp= document.getElementById("hp").value;
+console.log(hp);
+	
+
+	if (country == "Austria") {
+		var result = ((hp*100) / (age + 50)).toFixed(2);
+		 document.getElementById("result").innerHTML = `${name} your insurance price is ${result} €`;
+	
+	}
+	else if (country == "Hungary") {
+		var result = ((hp*120) / (age + 100)).toFixed(2);
+		document.getElementById("result").innerHTML = `${name} your insurance price is ${result}  €`;
+	}	
+
+	else if (country == "Greece") {
+		var result = ((hp*150) / (age + 3) + 50).toFixed(2);
+
+		document.getElementById("result").innerHTML = `${name} your insurance price is ${result} €`;
+	}	
+
+
+}
+
+document.getElementById("button").addEventListener("click", calcInsurance, false)
+
+	
+	
+
+
+
+
+
+	
+
